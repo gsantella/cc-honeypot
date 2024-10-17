f9eed4ee519ac4249a510db322c773e183fe398d
ColdTurkies <148791567+ColdTurkies@users.noreply.github.com>
2024-10-17 11:22:06 -0400

    user.ts / Added Dummy Routes
    Added
    ----------
    GET all users
    GET user by uuid
    POST one user
    DELETE user by uuid

--- src/routes/v1/user.ts
+++ src/routes/v1/user.ts
@@ -2,10 +2,20 @@ import express from 'express'
 
 const router = express.Router({ mergeParams: true })
 
+router.get('/user', (req, res) => {
+    res.send('GET all users.')
+  })
 
+router.get('/user/:uuid', (req, res) => {
+    res.send('GET one user by uuid.')
+  })
 
+router.post('/user', (req, res) => {
+    res.json(['POST one user.'])
+  })
 
+router.delete('/user', (req, res) => {
+    res.send('DELETE one user by uuid.')
+  })
 
-
-  
 export default router
