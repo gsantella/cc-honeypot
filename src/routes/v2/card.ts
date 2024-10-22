80fc43599a27a8a455d31f4a77394df5b7b60740
dstitt0516 <148791450+dstitt0516@users.noreply.github.com>
2024-10-21 23:14:21 -0400

    Revert "card endpoint stubs added"

--- src/routes/v2/card.ts
+++ src/routes/v2/card.ts
@@ -2,24 +2,10 @@ import express from 'express'
 
 const router = express.Router({ mergeParams: true })
 
-// GET /
-router.get('/', (req, res) => {
-  res.send('I will get all credit cards')
-})
 
-// GET /:uuid
-router.get('/:uuid', (req, res) => {
-  res.send('I will get all credit cards with this UUID')
-})
 
-// POST /
-router.post('/', (req, res) => {
-  res.send('I will create a credit card')
-})
 
-// DELETE /:uuid
-router.delete('/:uuid', (req, res) => {
-  res.send('I will delete a credit card with this UUID')
-})
 
+
+  
 export default router
