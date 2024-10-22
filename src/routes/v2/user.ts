e344c60daf09b3e2a92638b796ca50f5126e96cf
shawn <148699849+Oppslia@users.noreply.github.com>
2024-10-22 10:30:48 -0400

    Added user routes

--- src/routes/v2/user.ts
+++ src/routes/v2/user.ts
@@ -1,6 +1,30 @@
-import express from 'express'
+import express,{Request,Response} from "express";
 
 const router = express.Router({ mergeParams: true })
+
+router.use(express.json())
+
+router.get("/user/", (req: Request, res: Response) => {
+    res.json({message: {users: {"userID":"content"}}})
+    //gets all users? why would we need this?
+})
+
+router.get("/user/:uuid", (req: Request, res: Response) => {
+    res.json({message: {"userID":"content"}})
+    // Gets a user by ID
+})
+router.post("/user", (req: Request, res: Response) => {
+    res.json({message: `Created user UserID`})
+    //Creates user
+})
+router.delete("/user/:uuid", (req: Request, res: Response) => {
+    res.json({message: `Deleted user UserID`})
+// Deletes user
+})
+router.patch("/user/:uuid", (req: Request, res: Response) => {
+    res.json({message: `updated user UserID`})
+// updates user information
+})
 
 
 
