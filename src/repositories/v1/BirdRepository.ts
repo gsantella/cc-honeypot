5f0e672fc7471def81d4dc6feb4f6162767bdcbb
Guido Santella <gsantella@southhills.edu>
2024-10-22 17:59:14 -0400

    almost there

---
+++ src/repositories/v1/BirdRepository.ts
@@ -0,0 +1,14 @@
+import { db } from '../../db/database.js'
+
+export async function findBirdById(uuid: string) {
+  return await db.selectFrom('birds')
+    .where('id', '=', uuid)
+    .selectAll()
+    .executeTakeFirst()
+}
+
+export async function deleteBird(uuid: string) {
+  return await db.deleteFrom('birds').where('id', '=', uuid)
+    .returningAll()
+    .executeTakeFirst()
+}
--- src/routes/v1/bird.ts
+++ src/routes/v1/bird.ts
@@ -1,11 +1,16 @@
 import express from 'express'
+import * as BirdRepository from '../../repositories/v1/BirdRepository.js'
 
 const router = express.Router({ mergeParams: true })
 
 // define the home route
 router.get('/', (req, res) => {
     res.send('Birds home page')
-  })
+})
+
+router.get('/:uuid', (req, res) => {
+    console.log(BirdRepository.findBirdById('7c5f78e2-67e1-4dbc-a9be-e9bb196a256b'))
+})
 
 // define the about route
 router.get('/about', (req, res) => {
