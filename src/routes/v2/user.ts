6083a8afc5b7eee223d78712f3a89a8ea7277a52
shawn <148699849+Oppslia@users.noreply.github.com>
2024-10-23 21:21:21 -0400

    Delete Errorhandeling Fixes

--- src/routes/v2/user.ts
+++ src/routes/v2/user.ts
@@ -19,20 +19,33 @@ router.get("/:uuid", async (req: Request, res: Response) => {
 router.post("/", async (req: Request, res: Response) => {
     try{    //Creates user
         const user = await UserRepository.createUser(req.body)
+        console.log("lol2")
         res.status(201).json(user)
     } catch(error:any) {
+        console.log("lol3")
         if (error.message.includes("Invalid argument")) {
             res.status(400).json({ error: error.message })
         }
-        res.status(500).json({message:"Internal Server Error"})
+        res.status(500).json({error:error.message})
         console.error(error) // will never run until our data becomes more complex.
     }
 })
 router.delete("/:uuid", (req: Request, res: Response) => {
     try{    // Deletes user
-        let x = UserRepository.deleteUser(req.params.uuid)
-        res.json({message: x})
-    } catch(error) {console.error(error)}
+       const user = UserRepository.deleteUser(req.params.uuid)
+       res.status(201).json({deleted:user})
+    } catch(error:any) {
+        console.error(error)
+        if (error.message.includes("Invalid argument")) {
+            res.status(400).json({ error: error.message })
+        }
+        if (error.message.includes("ID not Found")) {
+            res.status(400).json({error: error.message})
+        } else {res.status(500).json({message:"Internal Server Error"})
+        }
+            
+        }
+        
 })
 router.patch("/:uuid", (req: Request, res: Response) => {
     try{    // updates user information
